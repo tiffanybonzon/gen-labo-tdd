cfbb23180170cac02955063f57a989948429b9d7
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 15:48:49 +0100

    [Make It N.57], Added reduce() to Sum, fixes #12

--- src/main/java/BookExample/code/Bank.java
+++ src/main/java/BookExample/code/Bank.java
@@ -3,7 +3,6 @@ package BookExample.code;
 public class Bank {
     public Money reduce(Expression source, String to) {
         Sum sum = (Sum) source;
-        int amount = sum.augend.amount + sum.addend.amount;
-        return new Money(amount, to);
+        return sum.reduce(to);
     }
 }
