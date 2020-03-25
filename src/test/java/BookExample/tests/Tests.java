ab454ee9ee6480d14bb9d589040e01d12d93507b
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 13:00:33 +0100

    [Makin' Objects N.37] Began refactor for times, fixes #8

--- src/test/java/BookExample/tests/Tests.java
+++ src/test/java/BookExample/tests/Tests.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Tests {
     @Test
     public void testMultiplication() {
-        Dollar five = new Dollar(5);
+        Dollar five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
