2168b969fa2fb694e8be3dafb0476659d1455344
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 12:30:58 +0100

    [Equality For All N.25] Generalized equals to pass testEquality(), closes #3

--- src/test/java/BookExample/tests/Tests.java
+++ src/test/java/BookExample/tests/Tests.java
@@ -18,5 +18,6 @@ public class Tests {
     @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
