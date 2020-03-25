651bfc57795b2e3ef30d84cbfeace3f5ec62d320
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 12:44:23 +0100

    [Equality For All, Redux N.33] Changed Dollar equals type to Money, fixes #6

--- src/main/java/BookExample/code/Dollar.java
+++ src/main/java/BookExample/code/Dollar.java
@@ -10,7 +10,7 @@ public class Dollar extends Money {
     }
 
     public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
+        Money dollar = (Dollar) object;
         return amount == dollar.amount;
     }
 }
