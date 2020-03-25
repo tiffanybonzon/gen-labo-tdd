04bd575ddb7ad3521c1e095d58c68e8fb49adf44
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 12:51:06 +0100

    [Equality For All, Redux N.34] Refactor Franc to be an extension on Money (just like Dollar), closes #6

--- src/main/java/BookExample/code/Franc.java
+++ src/main/java/BookExample/code/Franc.java
@@ -1,18 +1,11 @@
 package BookExample.code;
 
-public class Franc {
-    private int amount;
-
+public class Franc extends Money {
     public Franc(int amount) {
         this.amount = amount;
     }
 
     public Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 }
