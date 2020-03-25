57c589689013bf78e9407a62ec007950991335eb
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 12:29:16 +0100

    [Equality For All N.25] Worst possible way to pass testEquality(), fixes #3

--- src/test/java/BookExample/tests/Tests.java
+++ src/test/java/BookExample/tests/Tests.java
@@ -3,7 +3,7 @@ package BookExample.tests;
 import BookExample.code.*;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
     @Test
@@ -13,5 +13,10 @@ public class Tests {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
     }
 }
