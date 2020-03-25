9f8581d3b11fd7baa325e0e3cb25328484787360
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 16:51:28 +0100

    [Mixed Currencies N.66] Sum nos uses Expressions instead of Money for augend and addend, fixes #14

--- src/main/java/BookExample/code/Sum.java
+++ src/main/java/BookExample/code/Sum.java
@@ -1,10 +1,10 @@
 package BookExample.code;
 
 public class Sum implements Expression {
-    public Money augend;
-    public Money addend;
+    public Expression augend;
+    public Expression addend;
 
-    public Sum(Money augend, Money addend) {
+    public Sum(Expression augend, Expression addend) {
         this.augend = augend;
         this.addend = addend;
     }
