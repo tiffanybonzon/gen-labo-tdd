78de1c07da46a5045b80710aab9cced800d1f420
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 15:52:35 +0100

    [Make It N.58], Added testReduceMoney() + ***UGLY*** way to pass the newly created test, fixes #12

--- src/main/java/BookExample/code/Bank.java
+++ src/main/java/BookExample/code/Bank.java
@@ -2,6 +2,10 @@ package BookExample.code;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
+        if(source instanceof Money) {
+            return (Money) source;
+        }
+
         Sum sum = (Sum) source;
         return sum.reduce(to);
     }
