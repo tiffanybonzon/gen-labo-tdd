be9233f2796babf322c5ac162ea40533bc9997c5
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 12:11:26 +0100

    [Money Example N.18] Terrible way to pass testMultiplication(), fixes #1

--- src/main/java/BookExample/code/Dollar.java
+++ src/main/java/BookExample/code/Dollar.java
@@ -1,4 +1,11 @@
 package BookExample.code;
 
 public class Dollar {
+    public int amount = 10;
+
+    public Dollar(int amount) {
+    }
+
+    public void times(int multiplier) {
+    }
 }
--- src/test/java/BookExample/tests/Tests.java
+++ src/test/java/BookExample/tests/Tests.java
@@ -1,4 +1,15 @@
 package BookExample.tests;
 
+import BookExample.code.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Tests {
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
 }
