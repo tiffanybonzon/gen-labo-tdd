8ecdb86f191bcc7d2e4d4051fa0f66f56f7de10f
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 15:56:18 +0100

    [Make It N.59], Added reduce to Expression + removed uggly casts and class tests from Bank.reduce(), closes #12

--- src/main/java/BookExample/code/Bank.java
+++ src/main/java/BookExample/code/Bank.java
@@ -2,11 +2,6 @@ package BookExample.code;
 
 public class Bank {
     public Money reduce(Expression source, String to) {
-        if(source instanceof Money) {
-            return (Money) source;
-        }
-
-        Sum sum = (Sum) source;
-        return sum.reduce(to);
+        return source.reduce(to);
     }
 }
