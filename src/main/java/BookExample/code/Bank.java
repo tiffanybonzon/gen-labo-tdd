a254eb85a9ec69ba04edf8cbe97d3ef52f6d5187
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 16:18:44 +0100

    [Change N.63] testReduceMoneyDifferentCurrency() + pass - Pair class, fixes #13

--- src/main/java/BookExample/code/Bank.java
+++ src/main/java/BookExample/code/Bank.java
@@ -1,7 +1,19 @@
 package BookExample.code;
 
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public int rate(String from, String to) {
+        return (from.equals("CHF") & to.equals("USD")) ? 2 : 1;
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
--- src/main/java/BookExample/code/Expression.java
+++ src/main/java/BookExample/code/Expression.java
@@ -1,5 +1,5 @@
 package BookExample.code;
 
 public interface Expression {
-    Money reduce(String to);
+    Money reduce(Bank bank, String to);
 }
