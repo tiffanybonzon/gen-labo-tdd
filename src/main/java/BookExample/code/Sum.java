fa08519bff3d33bb9a211a9f80b9c09d9e2bdaaf
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 15:41:45 +0100

    [Make It N.57], testReduceSum() + Modified Bank.reduce() to pass test, fixes #12

--- src/main/java/BookExample/code/Sum.java
+++ src/main/java/BookExample/code/Sum.java
@@ -4,7 +4,7 @@ public class Sum implements Expression {
     public Money augend;
     public Money addend;
 
-    Sum(Money augend, Money addend) {
+    public Sum(Money augend, Money addend) {
         this.augend = augend;
         this.addend = addend;
     }
