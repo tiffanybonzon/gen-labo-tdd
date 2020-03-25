0a71b2dc9020d78232fe65ae4e11ed3d6bdd07a0
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 15:10:32 +0100

    [Addition, Finally N.51] testSimpleAddition() + plus method in Money, fixes #11

--- src/main/java/BookExample/code/Money.java
+++ src/main/java/BookExample/code/Money.java
@@ -34,4 +34,8 @@ public class Money {
     public String toString() {
         return amount + " " + currency;
     }
+
+    public Money plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
 }
