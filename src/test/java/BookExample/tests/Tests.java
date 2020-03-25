f651df9176e6bad71c5ac1841d054875b066c37b
Tiffany Bonzon <tiffany.bonzon@heig-vd.ch>
2020-03-25 14:41:14 +0100

    [The Root Of All Evil N.48] Replaced ref to subclass by ref to superclass, fixes #10

--- src/test/java/BookExample/tests/Tests.java
+++ src/test/java/BookExample/tests/Tests.java
@@ -24,7 +24,7 @@ public class Tests {
 
     @Test
     public void testFrancMultiplication() {
-        Franc five = Money.franc(5);
+        Money five = Money.franc(5);
         assertEquals(Money.franc(10), five.times(2));
         assertEquals(Money.franc(15), five.times(3));
     }
